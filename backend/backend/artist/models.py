a72f086d60524f899cef032f28e95c927599dfd4
Pavan Kusunuri <pavantejakusunuri@gmail.com>
2024-01-02 21:13:37 +0530

    Created a table for Artist with Artist Category as a foreign key

--- backend/backend/artist/models.py
+++ backend/backend/artist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from artist_category.models import ArtistCategory
 
 # Create your models here.
+class Artist(models.Model):
+    GENDER = {
+        "M": "Male",
+        "F": "Female",
+        "T": "Transgender",
+    }
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    current_location = models.CharField(max_length=100)
+    place_of_birth = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    artist_category = models.ForeignKey(ArtistCategory, on_delete = models.CASCADE)
